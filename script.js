b3a749fc743fd6f82c8e7536764bb94105c88d70
biedrinairbe <148642312+kadevandermerwe@users.noreply.github.com>
2023-11-09 17:09:47 +0000

    Added services section on homepage + responsive design

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+  if (document.body.scrollTop > 0.1 || document.documentElement.scrollTop > 0.1) {
     document.getElementById("nav").style.position = "fixed";
-    document.getElementById("Hero").style.padding = "100px 0px 0px 0px";
+    document.getElementById("Hero").style.padding = "75px 0px 0px 0px";
   } else {
     document.getElementById("nav").style.position = "relative";
     document.getElementById("Hero").style.padding = "0px 0px 0px 0px";
+
+  }
+
+  if (document.body.scrollTop > 580 || document.documentElement.scrollTop > 580) {
+    document.getElementById("nav").style.boxShadow = "0px 2px 2px rgba(0,0,0,0.2)";
+  } else {
+    document.getElementById("nav").style.boxShadow = "none";
 
   }
 }
